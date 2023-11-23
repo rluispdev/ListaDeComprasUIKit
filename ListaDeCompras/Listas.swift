53eee2480f4dfb7abff7f1e50ed40ebc44553cd1
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-11-23 19:52:47 -0300

    Primeiro Comit

---
+++ ListaDeCompras/Listas.swift
@@ -0,0 +1,37 @@
+//
+//  Listas.swift
+//  ListaDeCompras
+//
+//  Created by Rafael Gonzaga on 23/11/23.
+//
+
+import Foundation
+import UIKit
+
+//Delegate UIViewControler
+class List: UIViewController {
+    var v1 = 3
+    var v2 = 5
+    let v3 = 7
+    var string = "Hello!"
+    
+    override func viewDidLoad() {
+        v1 = v2 + v1
+        print(v1)
+        //Fazer Debugger
+        NSLog("olá %@ com o número %i", string, v1)
+        
+        if (v1 > v3){
+            NSLog("V1 é mmaior que v3 %i",   v1)
+        }
+        
+        let array = ["a", "b", "c"]
+        for i in array {
+            print(i)
+        }
+        
+        for i in 0..<v2 {
+            print(i)
+        }
+    }
+}
